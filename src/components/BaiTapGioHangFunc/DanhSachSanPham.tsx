4bfb3753de1207fb29178e378615bd78a65e4b2f
Nhan Vi Nam <nhanvinamcoder@gmail.com>
2020-09-25 15:28:36 +0700

    render san pham

--- src/components/BaiTapGioHangFunc/DanhSachSanPham.tsx
+++ src/components/BaiTapGioHangFunc/DanhSachSanPham.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import danhsachDTJSON from "../../utils/danhSachDienThoai.json";
+import DienThoaiType from "../../models/DienThoaiType";
+import SanPham from "./SanPham";
 const DanhSachSanPham: React.FC = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [danhSachDienThoai] = useState<DienThoaiType[]>(danhsachDTJSON);
+  const renderListDienThoai = (): JSX.Element[] | undefined => {
+    if (!danhSachDienThoai) return;
+    return danhSachDienThoai.map((sanpham) => {
+      return <SanPham sanpham={sanpham} key={sanpham.id} />;
+    });
+  };
+  return <div className="row text-center">{renderListDienThoai()}</div>;
+};
 
-export default DanhSachSanPham
+export default DanhSachSanPham;
--- src/components/BaiTapGioHangFunc/SanPham.tsx
+++ src/components/BaiTapGioHangFunc/SanPham.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
-const SanPham: React.FC  = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from "react";
+import DienThoaiType from "../../models/DienThoaiType";
+interface SanPhamProps {
+  sanpham: DienThoaiType;
+  key: number;
 }
+const SanPham: React.FC<SanPhamProps> = (props) => {
+  const { sanpham } = props;
+  const renderSanPham = (): JSX.Element => {
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={sanpham.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{sanpham.name}</h5>
+          <p className="card-text">{sanpham.description}</p>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-success mr-2">Mua</button>
+          <button className="btn btn-primary ml-2">Xem</button>
+        </div>
+      </div>
+    );
+  };
+  return <div className="col-lg-4">{renderSanPham()}</div>;
+};
 
-export default SanPham
+export default SanPham;
